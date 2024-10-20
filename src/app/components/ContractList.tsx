41660ba90df9bd8d3390bbae944f62ce0d3f91c9
Sourav <souravbansal277@gmail.com>
2024-10-20 13:59:45 +0530

    tailwind updates

--- src/app/components/ContractList.tsx
+++ src/app/components/ContractList.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 
 const ContractList = () => {
-  const [selectedValue, setSelectedValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
-  const options = ['NIFTYBANK', 'SENSEX', 'NIFTY50'];
+  const options = ["NIFTYBANK", "SENSEX", "NIFTY50"];
 
   const handleSelect = (value) => {
     setSelectedValue(value);
@@ -20,22 +20,26 @@ const ContractList = () => {
           {/* Selected value box */}
           <div
             className={`w-full bg-green-300 text-center text-gray-700 py-3 rounded-lg cursor-pointer ${
-              isOpen ? 'border border-green-500' : ''
+              isOpen ? "border border-green-500" : ""
             }`}
           >
-            {selectedValue ? selectedValue : 'Select an option'}
+            <span>{selectedValue ? selectedValue : "Select an option"}</span>
+            {/* Down arrow icon */}
+            <span className="m-2">{isOpen ? "▲" : "▼"}</span>
           </div>
 
           {/* Dropdown list */}
           {isOpen && (
-            <ul className="absolute w-full max-w-xs bg-white shadow-lg rounded-lg mt-1 z-10">
+            <ul className="absolute  max-w-xs bg-white shadow-lg rounded-lg mt-1 z-10 md:w-[full] sm:w-[40vw]">
               {options.map((option) => (
                 <li
                   key={option}
                   className="py-2 px-4 hover:bg-green-100 cursor-pointer text-center"
                   onClick={() => handleSelect(option)}
                 >
-                  <span className="block w-full whitespace-normal">{option}</span>
+                  <span className="block w-full whitespace-normal">
+                    {option}
+                  </span>
                 </li>
               ))}
             </ul>
